800f0ba2da0146b68a13a38de120fc139d18749a
Utkin Denis <stolen.youth@yandex.ru>
2022-08-31 14:31:05 +0700

    feat: backend

---
+++ index.js
@@ -0,0 +1,69 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+import cors from 'cors';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import multer from 'multer';
+
+import {registerValidation, loginValidation, postCreateValidation} from './validations.js';
+import UserModel from "./models/user.js";
+import { PostController, UserController } from "./controllers/index.js";
+import { handleValidationErrors, checkAuth } from "./utils/index.js";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('DB ok')
+}).catch((err) => {
+    console.log(err);
+})
+
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        if (!fs.existsSync('uploads')) {
+            fs.mkdirSync('uploads');
+        }
+        cb(null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.get('/tags', PostController.getLastTags);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `uploads/${req.file.originalname}`,
+    });
+});
+
+app.listen(process.env.PORT || 4444, (err) => {
+    if (err) {
+        return console.log(err)
+    }
+
+    console.log('Server OK')
+});
